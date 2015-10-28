fdd7f40c96ddcbf70e63db3ba95290f3a85d1b58
Joe Marley <joe.d.marley@seagate.com>
2015-10-28 11:37:35 -0600

    Added bare watching of directory.  No real changes: just a callback that happens when a file is added to or removed from the watched directory.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,46 +2,105 @@
 #include "ui_mainwindow.h"
 
 #include <QFileDialog>
+#include <QFileSystemWatcher>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow), watcher(nullptr)
 {
     ui->setupUi(this);
 
     connect(ui->action_Open, &QAction::triggered, this,
-            &MainWindow::SlotOpenWatch);
+            &MainWindow::slotOpenWatch);
     connect(ui->action_Close, &QAction::triggered, this,
-            &MainWindow::SlotCloseWatch);
+            &MainWindow::slotCloseWatch);
 
-    SlotCloseWatch();
+    slotCloseWatch();
 }
 
 MainWindow::~MainWindow() { delete ui; }
 
-void MainWindow::SlotOpenWatch()
+void MainWindow::slotOpenWatch()
 {
-    QString dir = QFileDialog::getExistingDirectory(
+    QString path = QFileDialog::getExistingDirectory(
         this, tr("Watch Directory"), "",
         QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
 
-    if (dir.isEmpty())
+    if (path.isEmpty())
     {
         return;
     }
 
-    SlotCloseWatch();
+    slotCloseWatch();
 
-    // TODO : Start watching directory here
+    watcher = new QFileSystemWatcher(this);
+    connect(watcher, &QFileSystemWatcher::directoryChanged, this,
+            &MainWindow::slotDirectoryChanged);
+    connect(watcher, &QFileSystemWatcher::fileChanged, this,
+            &MainWindow::slotFileChanged);
+    watcher->addPath(path);
 
+    slotDirectoryChanged(path);
     ui->listWidget->show();
     ui->plainTextEdit->show();
 }
 
-void MainWindow::SlotCloseWatch()
+void MainWindow::slotCloseWatch()
 {
+    if (nullptr != watcher)
+    {
+        watcher->deleteLater();
+    }
+
     ui->listWidget->hide();
     ui->listWidget->clear();
 
     ui->plainTextEdit->hide();
     ui->plainTextEdit->clear();
 }
+
+void MainWindow::slotDirectoryChanged(const QString &path)
+{
+    auto watchedFiles = watcher->files();
+    watchedFiles.sort();
+
+    QDir dir(path);
+    auto allFiles = dir.entryList(QDir::Files, QDir::Name);
+    for (auto& file : allFiles)
+    {
+        file = dir.absoluteFilePath(file);
+    }
+
+    auto iterWatch = watchedFiles.begin();
+    auto iterAll = allFiles.begin();
+    while (!(iterWatch == watchedFiles.end() && iterAll == allFiles.end()))
+    {
+        if (iterWatch == watchedFiles.end())
+        {
+            // TODO : Remove watch on file.
+            ++iterAll;
+        }
+        else if (iterAll == allFiles.end())
+        {
+            // TODO : Add watch on file.
+            ++iterWatch;
+        }
+        else if ((*iterAll) < (*iterWatch))
+        {
+            // TODO : Remove watch on file.
+            ++iterAll;
+        }
+        else if ((*iterAll) > (*iterWatch))
+        {
+            // TODO : Add watch on file.
+            ++iterWatch;
+        }
+        else // items match
+        {
+            ++iterAll;
+            ++iterWatch;
+        }
+    }
+}
+
+void MainWindow::slotFileChanged(const QString &path) {}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+class QFileSystemWatcher;
 
 namespace Ui {
 class MainWindow;
@@ -16,11 +17,18 @@ public:
     ~MainWindow();
 
 public slots:
-    void SlotCloseWatch();
-    void SlotOpenWatch();
+    void slotCloseWatch();
+    void slotOpenWatch();
+    void slotDirectoryChanged(const QString& path);
+    void slotFileChanged(const QString& path);
 
 private:
+
+    void addWatch(const QString& path);
+    void delWatch(const QString& path);
+
     Ui::MainWindow *ui;
+    QFileSystemWatcher *watcher;
 };
 
 #endif // MAINWINDOW_H
