cda3c24e6990137925dce2dc5a78aaac2eb7ed49
Joe Marley <joe.d.marley@seagate.com>
2015-10-28 10:51:31 -0600

    Add slots to MainWindow for Open/CloseWatch.  Connect triggers from actions to slots.  Add dialog to get a directory name.  Add icons.  No actual directory watching at this commit.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->action_Open, &QAction::triggered, this,
+            &MainWindow::SlotOpenWatch);
+    connect(ui->action_Close, &QAction::triggered, this,
+            &MainWindow::SlotCloseWatch);
+
+    SlotCloseWatch();
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::SlotOpenWatch()
 {
-    delete ui;
+    QString dir = QFileDialog::getExistingDirectory(
+        this, tr("Watch Directory"), "",
+        QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+    if (dir.isEmpty())
+    {
+        return;
+    }
+
+    SlotCloseWatch();
+
+    // TODO : Start watching directory here
+
+    ui->listWidget->show();
+    ui->plainTextEdit->show();
+}
+
+void MainWindow::SlotCloseWatch()
+{
+    ui->listWidget->hide();
+    ui->listWidget->clear();
+
+    ui->plainTextEdit->hide();
+    ui->plainTextEdit->clear();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void SlotCloseWatch();
+    void SlotOpenWatch();
+
 private:
     Ui::MainWindow *ui;
 };
